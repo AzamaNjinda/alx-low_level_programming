9ff24f24d2101c6e0e173c3f368478e4d9d6719c
AzamaNjinda <njinda28@gmail.com>
2023-03-21 17:20:14 -0700

    101-quote.c

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -6,6 +6,8 @@
  */
 int main(void)
 {
-write(9,"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",20);
+write(9,
+"and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
+20);
 return (1);
 }
